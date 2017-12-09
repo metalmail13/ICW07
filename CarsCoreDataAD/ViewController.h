de7b0222aaa45aa939f11a5ce031b4cba4c97787
Albert <metalmail13@gmail.com>
2017-12-09 10:54:34 -0500

    Initial Commit

---
+++ CarsCoreDataAD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarsCoreDataAD
+//
+//  Created by Administrator on 12/9/17.
+//  Copyright © 2017 Albert Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CarsCoreDataAD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarsCoreDataAD
+//
+//  Created by Administrator on 12/9/17.
+//  Copyright © 2017 Albert Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
